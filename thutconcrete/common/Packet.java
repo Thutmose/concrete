8d57c9c87f6f85a823ccd4e6ecdb9912f43f563d
Thutmose <elpatricimo@gmail.com>
2013-05-12 16:45:15 -0400

    more removals

--- thutconcrete/common/Packet.java
+++
@@ -1,9 +0,0 @@
-package thutconcrete.common;
-
-import java.io.DataInputStream;
-
-import net.minecraft.entity.player.EntityPlayer;
-
-public abstract class Packet {
-	public abstract void handle(DataInputStream iStream, EntityPlayer player);
-}
